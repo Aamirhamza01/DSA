8406e84370af01383f0a487cd6708f8fe05f17b3
Aamir-hamza <anasturk14287@gamil.com>
2026-08-24 13:22:42 +0530

    DSA

---
+++ Day04.cpp
@@ -0,0 +1,82 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     int height[]={2,3,4,56,54,21,23};
+//     int n=7;
+//     int maxWater=0;
+//     int lp=0 ;
+
+//     while (lp<rp){
+     
+//         int w=rp-lp;
+//         int ht=min(height[lp],height[rp]);
+//         int currWater=w*ht;
+//         int maxWater=max(maxWater,currWater);
+        
+//         height[lp]<height[rp]?lp++:rp--;
+      
+//     }
+//     return maxWater;
+    
+
+
+
+// }
+
+
+//Product of Array Except Self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
