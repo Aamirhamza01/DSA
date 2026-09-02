0a218afcd8b9e81d0d83154efae1a5945e845d39
Aamir-hamza <anasturk14287@gamil.com>
2026-09-02 21:55:43 +0530

    day06

---
+++ Day-06/Day06.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// int binarysearch(vector<int> arr)
+// {
+//     int st = 1, end = arr.size() - 2;
+
+//     while (st <= end)
+//     {
+//         int mid = st + (end - st) / 2;
+
+//         if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+//         {
+//             return arr[mid];
+//         }
+//         else if (arr[mid - 1] < arr[mid])
+//         {
+//             st = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {3, 4, 5, 9, 15, 12, 10, 8, 5};
+
+//     cout << binarysearch(arr) << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
