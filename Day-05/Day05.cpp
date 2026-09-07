981d42f09a9d85f3e06d0faa5aabb17a38617855
Aamir-hamza <anasturk14287@gamil.com>
2026-09-07 20:46:48 +0530

    More

---
+++ Day-05/Day05.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     int n =5;
+//     int sum =0;
+//     for(int i=0 ; i<n; i++){
+//         sum+=arr[i];
+//     }
+//     cout<<sum<<endl;
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter array lenght :" << endl;
+//     cin >> n;
+//     int sum = 0;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     cout << sum << endl;
+    
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+
+//     cout << "Enter array length: " << endl;
+//     cin >> n;
+
+//     int sum = 0;
+//     int arr[n];
+
+//     // Input
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // Sum
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//     }
+
+//     cout << "Sum = " << sum << endl;
+
+//     // Print array
+//     cout << "Array = ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// int binarysearch(vector<int>arr ,int tar){
+//     int st=0,end=arr.size()-1;
+
+//     while(st<=end){
+//        int mid=(st+end)/2;
+//        if(tar>arr[mid]){
+//         st=mid+1;
+//        }else if(tar<arr[mid]){
+//         end=mid-1;
+//        }else{
+//         return mid;
+//        }
+//     }
+//     return -1;
+
+// }
+
+// int main(){
+//     vector<int>arr1={-1,0,3,4,5,9,12};
+//     int tar1=12;
+//     cout<<binarysearch(arr1,tar1)<<endl;
+//     vector<int>arr2={-1,0,3,5,9,12};
+//     int tar2=0;
+//     cout<<binarysearch(arr2,tar2)<<endl;
+
+    
+
+//     return 0;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
