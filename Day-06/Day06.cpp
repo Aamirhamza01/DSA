e78d3f76f2e50ed98eea308124c492fa16fad64a
Aamir-hamza <anasturk14287@gamil.com>
2026-09-08 21:49:08 +0530

    DSA06

--- Day-06/Day06.cpp
+++ Day-06/Day06.cpp
@@ -76,68 +76,68 @@ using namespace std;
 //     return 0;
 // }
 
-bool isValid(vector<int> &arr, int n, int m, int maxallowedpage)
-{
-    int stu = 1, pages = 0;
+// bool isValid(vector<int> &arr, int n, int m, int maxallowedpage)
+// {
+//     int stu = 1, pages = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > maxallowedpage)
-        {
-            return false;
-        }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] > maxallowedpage)
+//         {
+//             return false;
+//         }
 
-        if (pages + arr[i] <= maxallowedpage)
-        {
-            pages += arr[i];
-        }
-        else
-        {
-            stu++;
+//         if (pages + arr[i] <= maxallowedpage)
+//         {
+//             pages += arr[i];
+//         }
+//         else
+//         {
+//             stu++;
 
-            pages = arr[i];
-        }
-    }
-    return stu > m ? false : true;
-}
+//             pages = arr[i];
+//         }
+//     }
+//     return stu > m ? false : true;
+// }
 
-int bookcase(vector<int> &arr, int n, int m)
-{
-    if (m > n)
-    {
-        return -1;
-    }
+// int bookcase(vector<int> &arr, int n, int m)
+// {
+//     if (m > n)
+//     {
+//         return -1;
+//     }
 
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    int ans = -1;
-    int st = 0, end = sum;
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     int ans = -1;
+//     int st = 0, end = sum;
 
-    while (st <= end)
-    {
-        int mid = st + (end - st) / 2;
+//     while (st <= end)
+//     {
+//         int mid = st + (end - st) / 2;
 
-        if (isValid(arr, n, m, mid))
-        {
-            ans = mid;
-            end = mid - 1;
-        }
-        else
-        {
-            st = mid + 1;
-        }
-    }
-    return ans;
-}
+//         if (isValid(arr, n, m, mid))
+//         {
+//             ans = mid;
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             st = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
 
-int main()
-{
-    vector<int> arr = {2, 1, 3, 4};
-    int n = 4, m = 2;
+// int main()
+// {
+//     vector<int> arr = {2, 1, 3, 4};
+//     int n = 4, m = 2;
 
-    cout << bookcase(arr, n, m) << endl;
-    return 0;
-}
+//     cout << bookcase(arr, n, m) << endl;
+//     return 0;
+// }
