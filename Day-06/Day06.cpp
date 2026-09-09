614b270685b7dbece5e33d8b8e4739f271802eeb
Aamir-hamza <anasturk14287@gamil.com>
2026-09-09 20:55:30 +0530

    comment

--- Day-06/Day06.cpp
+++ Day-06/Day06.cpp
@@ -186,50 +186,50 @@ using namespace std;
 //     return 0;
 // }
 
-bool isPossible(vector<int> &arr, int n, int c, int minimumallow)
-{
-    int cows = 1, last = arr[0];
+// bool isPossible(vector<int> &arr, int n, int c, int minimumallow)
+// {
+//     int cows = 1, last = arr[0];
 
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] - last >= minimumallow)
-        {
-            cows++;
-            last = arr[i];
-        }
-        if (cows == c)
-        {
-            return true;
-        }
-    }
-    return false;
-}
+//     for (int i = 1; i < n; i++)
+//     {
+//         if (arr[i] - last >= minimumallow)
+//         {
+//             cows++;
+//             last = arr[i];
+//         }
+//         if (cows == c)
+//         {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
-int getdistance(vector<int> &arr, int n, int c)
-{
-    sort(arr.begin(), arr.end());
-    int st = 1, end = arr[n - 1] - arr[0], ans = -1;
-    while (st <= end)
-    {
-        int mid = st + (end - st) / 2;
-        if (isPossible(arr, n, c, mid))
-        {
-            ans = mid;
-            st = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
-    }
-    return ans;
-}
+// int getdistance(vector<int> &arr, int n, int c)
+// {
+//     sort(arr.begin(), arr.end());
+//     int st = 1, end = arr[n - 1] - arr[0], ans = -1;
+//     while (st <= end)
+//     {
+//         int mid = st + (end - st) / 2;
+//         if (isPossible(arr, n, c, mid))
+//         {
+//             ans = mid;
+//             st = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
 
-int main()
-{
-    int n = 5, c = 3;
-    vector<int> arr = {1, 2, 3, 8, 4, 9};
+// int main()
+// {
+//     int n = 5, c = 3;
+//     vector<int> arr = {1, 2, 3, 8, 4, 9};
 
-    cout << getdistance(arr, n, c) << endl;
-    return 0;
-}
+//     cout << getdistance(arr, n, c) << endl;
+//     return 0;
+// }
