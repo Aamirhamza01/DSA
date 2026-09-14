c8e51d7908e186032800a34b89bebf8acba2cc0e
Aamir-hamza <anasturk14287@gamil.com>
2026-09-14 14:43:19 +0530

    Buble_sort

--- Day-06/Day06.cpp
+++ Day-06/Day06.cpp
@@ -252,23 +252,48 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    int arr[]={20,30,40,50,60,70,80};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int key=90;
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(arr[mid]==key){
-            cout<<"key :"<<key<<endl;
-            return 0;
-        }else if(arr[mid]==key){
-            low=mid+1;
-        }else{
-            high=mid-1;
+// int main(){
+//     int arr[]={20,30,40,50,60,70,80};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     int key=90;
+//     int low=0;
+//     int high=n-1;
+//     while(low<=high){
+//         int mid=low+(high-low)/2;
+//         if(arr[mid]==key){
+//             cout<<"key :"<<key<<endl;
+//             return 0;
+//         }else if(arr[mid]==key){
+//             low=mid+1;
+//         }else{
+//             high=mid-1;
+//         }
+//     }
+//     cout<<"Not Found"<<endl;
+//     return 0;
+// }
+
+int main() {
+    int arr[] = {40, 10, 30, 20, 50};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    cout<<"Not Found"<<endl;
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
