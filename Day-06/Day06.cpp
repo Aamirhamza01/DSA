5fc8eb16a79094de652596b2a553d2aebbf09a46
Aamir-hamza <anasturk14287@gamil.com>
2026-09-14 14:01:45 +0530

    Binay

--- Day-06/Day06.cpp
+++ Day-06/Day06.cpp
@@ -233,3 +233,42 @@ using namespace std;
 //     cout << getdistance(arr, n, c) << endl;
 //     return 0;
 // }
+
+
+// int main() {
+//     int arr[] = {10, 25, 15, 30, 35, 40};
+//     int target = 35;
+//     int n = 6;
+
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] == target) {
+//             cout << "Element found at index: " << i <<endl<<"at index Value :"<<arr[i]<< endl;
+//             return 0;
+//         }
+//     }
+
+//     cout << "Element not found" << endl;
+
+//     return 0;
+// }
+
+int main(){
+    int arr[]={20,30,40,50,60,70,80};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int key=90;
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==key){
+            cout<<"key :"<<key<<endl;
+            return 0;
+        }else if(arr[mid]==key){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    cout<<"Not Found"<<endl;
+    return 0;
+}
