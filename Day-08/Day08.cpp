831e00bdec37ed0a3c337b2bc38f0684790427eb
Aamir-hamza <anasturk14287@gamil.com>
2026-09-21 14:17:04 +0530

    Linklists

--- Day-08/Day08.cpp
+++ Day-08/Day08.cpp
@@ -1,7 +1,41 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+int main() {
+
+    // Nodes create
+    Node* first = new Node();
+    Node* second = new Node();
+    Node* Third = new Node();
+    Node* Fourth = new Node();
+
+// Data assign
+    first->data = 10;
+    second->data = 20;
+    Third->data = 30;
+    Fourth->data = 40;
+
+    // Nodes connect
+    first->next = second;
+    second->next = Third;
+    Third->next = Fourth;
+    Fourth->next = NULL;
+
+    // Linked List print
+    Node* temp = first;
+
+    while (temp != NULL) {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+
+    cout << "NULL";
+
+    return 0;
 }
